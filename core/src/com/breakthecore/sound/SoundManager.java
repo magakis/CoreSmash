5e1330ea8dd8e02e6bcd456839e2257b27750009
Michail Angelos Gakis <michael.gk95@gmail.com>
2018-06-29 15:40:39 +0300

    added a placeholder for sound-manager

---
+++ core/src/com/breakthecore/sound/SoundManager.java
@@ -0,0 +1,12 @@
+package com.breakthecore.sound;
+
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.utils.ObjectMap;
+
+public class SoundManager {
+    ObjectMap<String, Sound> sounds;
+
+    public SoundManager() {
+
+    }
+}
