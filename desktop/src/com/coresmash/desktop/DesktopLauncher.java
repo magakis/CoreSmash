e03275a45513ccaa08bbdf0dce91ee858a7f5c47
Michail Angelos Gakis <michael.gk95@gmail.com>
2018-08-03 00:12:32 +0300

    inserted again the DesktopLauncher in VC

---
+++ desktop/src/com/coresmash/desktop/DesktopLauncher.java
@@ -0,0 +1,52 @@
+package com.coresmash.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.coresmash.AdManager;
+import com.coresmash.CoreSmash;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+//		int height = 1920;
+//		int width = 1080;
+        int height = 1024;
+        int width = 768;
+
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+//		config.resizable = false;
+		config.y = 0;
+		if (CoreSmash.DEBUG_TABLET) {
+			config.height = width;
+			config.width = height;
+		} else {
+			config.height = height;
+			config.width = width;
+		}
+        config.overrideDensity = 220;
+		config.foregroundFPS = 60;
+		config.backgroundFPS = 15;
+		config.vSyncEnabled = false;
+		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
+		new LwjglApplication(new CoreSmash(new AdManager() {
+			@Override
+			public void show() {
+
+			}
+
+			@Override
+			public void showAdForReward(AdRewardListener listener) {
+
+			}
+
+			@Override
+			public void hide() {
+
+			}
+
+			@Override
+			public void toggle() {
+
+			}
+		}), config);
+	}
+}
