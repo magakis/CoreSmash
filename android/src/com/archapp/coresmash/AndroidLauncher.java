efc0c6406374fc56c898c4863e9bcd0c73cca014
Michail Angelos Gakis <michael.gk95@gmail.com>
2018-08-18 23:52:03 +0300

    added basic permission request for android

--- android/src/com/archapp/coresmash/AndroidLauncher.java
+++ android/src/com/archapp/coresmash/AndroidLauncher.java
@@ -1,6 +1,13 @@
 package com.archapp.coresmash;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.RelativeLayout;
 
@@ -13,6 +20,8 @@ public class AndroidLauncher extends AndroidApplication {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ensurePermissionsGranted();
+
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         config.useWakelock = true;
         RelativeLayout layout = new RelativeLayout(this);
@@ -22,5 +31,29 @@ public class AndroidLauncher extends AndroidApplication {
         layout.addView(gameView);
         adManager.init(this, layout);
         setContentView(layout);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void ensurePermissionsGranted() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            }, 1); // This 1 is app defined as an "internal" code for handling this specific request
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults.length > 0) {
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    exit();
+                    return;
+                }
+            }
+        } else {
+            exit();
+        }
     }
 }
